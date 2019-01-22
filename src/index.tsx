bae045e1436f0c997a134e48d5c210703bf4336e
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-01-22 15:22:26 -0400

    Refactored to class components

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.StatelessComponent<{}> = () => {
-    return <div>Hi there!</div>
+class App extends React.Component<any, any> {
+	constructor(props:any) {
+        super(props);
+
+        this.state = { lat: null, errMsg: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({ errMsg: err.message})
+        );
+    };
+
+	public render() {
+
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errMsg}
+            </div>
+        );
+    };
 };
+
 
 ReactDOM.render(
     <App />,
